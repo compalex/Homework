1077c013c66da6be879cb0a2ca5cc8fc852f1c42
compalex <47093016+compalex@users.noreply.github.com>
2019-02-26 16:16:56 -0500

    Add files via upload

--- Assignment2.java
+++ Assignment2.java
@@ -1,58 +1,77 @@
 package hw;
 
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
 /*
 * Assignment 2
 * @author Aliaksandr Yarmak
 */
 public class Assignment2 {
+    private static double bestAverage = 0; // optional task #2
+    private static int bestTeamId = 0; // optional task #2
 
     public static void main(String[] args) {
+        Scanner sc = null;
 
-        int idNum = 0, winNum = 0, losNum = 0;
-        int teamsQuant = 0;
-        Scanner sc;
+        try {
+            sc = new Scanner(new File("teams.txt"));
+            int teamsCount = 0;
 
-        for (int i = 0; i <= teamsQuant; i++) {
-
-            try {
-                sc = new Scanner(new File("teams.txt"));
-                for (int j = 0; j < i; j++) {
-                    sc.nextLine();
-                }
-                idNum = sc.nextInt();
-                winNum = sc.nextInt();
-                losNum = sc.nextInt();
-                System.out.printf("Team %d \n%d wins\t%d losses\n", idNum, winNum, losNum);
-
-                int totalNum = winNum + losNum;
-                System.out.printf("Total number of games played is %d\t%d games remaining\n", totalNum, 25 - totalNum);
-
-                double winAverage = (double) winNum / totalNum;
-                System.out.printf("The winning average is %.4f\n", winAverage);
-
-                if (winNum <= (25 - totalNum)) {
-                    System.out.println("Number of games remaining is greater than or equal to number won.");
-                } else
-                    System.out.println("Number of games remaining is less than number won.");
-
-                if (losNum < (25 - totalNum)) {
-                    System.out.println("Number of games remaining is greater than number lost.");
-                } else
-                    System.out.println("Number of games remaining is less than or equal to number won.");
-                teamsQuant++;
-
-            } catch (FileNotFoundException e) {
-                System.out.println("File is not found");
-            } catch (NoSuchElementException e) {
-                System.out.println("List of teams is ended, there are " + teamsQuant + " teams totally");
+            while (sc.hasNextLine()) {
+                ++teamsCount;               
+                int id = sc.nextInt();
+                int wins = sc.nextInt();
+                int loses = sc.nextInt();               
+                showTeamInfo(id, wins, loses);        
             }
-
-            System.out.println("\n");
+            System.out.println("List of teams is ended, there are " + teamsCount + " teams totally");
+            System.out.printf("The best average that is equal %.4f which belongs to the team #%d", bestAverage, bestTeamId);
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        } finally {
+            if (sc != null) sc.close();
         }
+    }
+
+    private static void showTeamInfo(int id, int wins, int losses) {
+        System.out.printf("Team %d \n%d wins\t%d losses\n", id, wins, losses);
+
+        int totalNum = wins + losses;
+        System.out.printf("Total number of games played is %d\t%d games remaining\n", totalNum, 25 - totalNum);
+
+        double winAverage = (double) wins / totalNum;
+        System.out.printf("The winning average is %.4f\n", winAverage);
+
+        if (wins <= (25 - totalNum)) {
+            System.out.println("Number of games remaining is greater than or equal to number won.");
+        } else {
+            System.out.println("Number of games remaining is less than number won.");
+        }
+
+        if (wins < (25 - totalNum)) {
+            System.out.println("Number of games remaining is greater than number lost.");
+        } else {
+            System.out.println("Number of games remaining is less than or equal to number won.");
+        }
+        
+        if (totalNum < 25) { // optional task #1
+            System.out.println("If this team wins the remaining games, the team's record will be:");
+            int newWins = wins + 25 - totalNum;
+            double newWinAverage = (double) newWins / 25;
+            System.out.printf("%d wins\t%d losses %.4f winning average\n", newWins, losses, newWinAverage);
+            
+            System.out.println("If this team loses the remaining games, the team's record will be:");
+            int newLosses = losses + 25 - totalNum;
+            newWinAverage = (double) wins / 25;
+            System.out.printf("%d wins\t%d losses %.4f winning average\n", wins, newLosses, newWinAverage);
+        }
+        
+        if (winAverage > bestAverage && winAverage != 1) { // optional task #2
+            bestAverage = winAverage;
+            bestTeamId = id;
+        }
+        System.out.println();                        
     }
 }
