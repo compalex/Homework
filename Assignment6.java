e100e4592da25cbac684e20976762b92b121e15e
compalex <47093016+compalex@users.noreply.github.com>
2019-04-27 17:39:55 -0400

    Add files via upload

--- Assignment6.java
+++ Assignment6.java
@@ -11,8 +11,8 @@ public class Assignment6 {
     public static final String ARRAY_PROMPT_MSG = "Please, input what elements the array has";
     public static final String FIRST_ARR_MSG = "Here's the original array:";
     public static final String SECOND_ARR_MSG = "Here is the new array:";
-    public static final String AVERAGE_MSG = "Average of array is %.3f\n";
-    public static Scanner sc = null;
+    public static final String AVERAGE_MSG = "Average of array is %.3f";
+    private static Scanner sc = null;
 
     public static void main(String[] args) {
         sc = new Scanner(System.in);
@@ -20,18 +20,19 @@ public class Assignment6 {
         try {
             System.out.println(SIZE_PROMPT_MSG);
             int size = sc.nextInt();
-            double[] firstArr = new double[size];
+            double[] firstArr = new double[size];            
             System.out.println(ARRAY_PROMPT_MSG);
             firstArr = readData(size, firstArr);
             System.out.println(FIRST_ARR_MSG);
             printArray(size, firstArr);
-            double avrg = findAverage(size, firstArr);
-            System.out.printf(AVERAGE_MSG, avrg);
-            double[] secondArr = howFarAway(size, avrg, firstArr);
+            double avg = findAverage(size, firstArr);
+            System.out.printf(AVERAGE_MSG, avg);
+            System.out.println();
+            double[] secondArr = howFarAway(size, avg, firstArr);
             System.out.println(SECOND_ARR_MSG);
             printArray(size, secondArr);
-            avrg = findAverage(size, secondArr);
-            System.out.printf(AVERAGE_MSG, avrg);
+            avg = findAverage(size, secondArr);
+            System.out.printf(AVERAGE_MSG, avg);
         } catch (InputMismatchException e) {
             System.err.println(e);
         } finally {
@@ -39,14 +40,14 @@ public class Assignment6 {
         }
     }
 
-    public static double[] readData(int n, double[] numbers) {
+    private static double[] readData(int n, double[] numbers) {
         for (int i = 0; i < n; i++) {
             numbers[i] = sc.nextDouble();
         }
         return numbers;
     }
 
-    public static void printArray(int q, double[] numb) {
+    private static void printArray(int q, double[] numb) {
         for (int i = 0; i < q; i++) {
             if (i % 5 == 0 && i != 0) {
                 System.out.println();
@@ -56,20 +57,20 @@ public class Assignment6 {
         System.out.println();
     }
 
-    public static double findAverage(int k, double[] p) {
-        int sum = 0;
+    private static double findAverage(int k, double[] p) {
+        double sum = 0;
 
         for (int i = 0; i < k; i++) {
             sum += p[i];
         }
-        return (double) sum / k;
+        return sum / k;
     }
 
-    public static double[] howFarAway(int m, double avrg, double[] r) {
+    private static double[] howFarAway(int m, double avg, double[] r) {
         double[] s = new double[m];
 
         for (int i = 0; i < m; i++) {
-            s[i] = r[i] - avrg;
+            s[i] = r[i] - avg;
         }
         return s;
     }
