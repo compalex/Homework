0f295c7b7834fda14c80b6fa717573054edf6fe5
compalex <47093016+compalex@users.noreply.github.com>
2019-04-27 17:36:20 -0400

    Add files via upload

--- Assignment5.java
+++ Assignment5.java
@@ -2,19 +2,23 @@ package hw;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 /*
 * Assignment 5
 * @author Aliaksandr Yarmak
 */
+enum Result {
+    WIN, LOSS, CONTINUE, EXIT
+}
 public class Assignment5 {
-    public static final String PROMPT_MSG = "Input 2 integer values in the range of 1 to 6.";
-    public static final String INPUT_DISPLAY = "Your input: %d, %d.\n";
+    public static final String PROMPT_MSG = "Input 2 integer values in the range of 1 to 6. (0 to exit)";
+    public static final String INPUT_DISPLAY = "Your input: %d, %d.";
     public static final String WIN_MSG = "Congratulations! You win!";
     public static final String LOSS_MSG = "Sorry, but you lose!";
     public static final String CONTINUATION_MSG = "You don't win or lose, so let's continue the game.";
-    public static final String EXIT_MSG = "To stop the game, input 0.";
-    public static final String ERROR_MSG = "Error! Please, enter what the program asks!";
-    public static Scanner sc = null;
+    public static final String EXIT_MSG = "The game is over";
+    public static final String ERROR_MSG = "Error: Invalid input";
+    private static Scanner sc = null;
 
     public static void main(String[] args) {
         boolean needToRepeat = true;
@@ -25,16 +29,24 @@ public class Assignment5 {
                 int die1 = sc.nextInt();
                 int die2 = sc.nextInt();
                 System.out.printf(INPUT_DISPLAY, die1, die2);
-                
-                if (die1 > 0 && die1 < 7 && die2 > 0 && die2 < 7) {
-                    String result = outcome(die1, die2);
-                    System.out.println(result);
-                    if (result == CONTINUATION_MSG) {
+                System.out.println();
+
+                if (isValidInput(die1, die2)) {
+                    Result result = outcome(die1, die2);
+                    switch (result) {
+                    case WIN:
+                        System.out.println(WIN_MSG);
+                        break;
+                    case LOSS:
+                        System.out.println(LOSS_MSG);
+                        break;
+                    case CONTINUE:
+                        System.out.println(CONTINUATION_MSG);
                         continuation(die1 + die2);
+                    case EXIT:
+                        System.out.println(EXIT_MSG);
+                        needToRepeat = false;
                     }
-                    System.out.println(EXIT_MSG);
-                } else if (die1 == 0 || die2 == 0) {
-                    needToRepeat = false;
                 } else {
                     System.out.println(ERROR_MSG);
                 }
@@ -46,26 +58,31 @@ public class Assignment5 {
         }
     }
 
-    public static String outcome(int d1, int d2) {
-        if (d1 + d2 == 7 || d1 + d2 == 11) {
-            return WIN_MSG;
-        } else if (d1 + d2 == 2 || d1 + d2 == 12) {
-            return LOSS_MSG;
+    private static Result outcome(int d1, int d2) {
+        int sum = d1 + d2;
+        if (d1 == 0 || d2 == 0) {
+            return Result.EXIT;
+        } else if (sum == 2 || sum == 12) {
+            return Result.LOSS;
+        } else if (sum == 7 || sum == 11) {
+            return Result.WIN;
         } else {
-            return CONTINUATION_MSG;
+            return Result.CONTINUE;
         }
     }
 
-    public static void continuation(int sum) {
+    private static void continuation(int sum) {
         System.out.println(PROMPT_MSG);
         int dice1 = sc.nextInt();
         int dice2 = sc.nextInt();
+        int currentSum = dice1 + dice2;
         System.out.printf(INPUT_DISPLAY, dice1, dice2);
-        
-        if (dice1 > 0 && dice1 < 7 && dice2 > 0 && dice2 < 7) {
-            if (dice1 + dice2 == 7) {
+        System.out.println();
+
+        if (isValidInput(dice1, dice2)) {
+            if (currentSum == 7) {
                 System.out.println(LOSS_MSG);
-            } else if (dice1 + dice2 == sum) {
+            } else if (currentSum == sum) {
                 System.out.println(WIN_MSG);
             } else {
                 System.out.println(CONTINUATION_MSG);
@@ -74,6 +91,14 @@ public class Assignment5 {
         } else {
             System.out.println(ERROR_MSG);
             continuation(sum);
+        }
+    }
+
+    private static boolean isValidInput(int dice1, int dice2) {
+        if (dice1 >= 0 && dice1 <= 6 && dice2 >= 0 && dice2 <= 6) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
